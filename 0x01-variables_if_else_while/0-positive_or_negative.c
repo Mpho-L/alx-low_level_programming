6e262e9da8871bead0ffb10e5186be69d70a9103
Mpho <mpholeqoalane@gmail.com>
2023-04-21 09:50:06 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,9 @@
  * main - Entry point of the program
  *
  * This function generates a random number and prints whether it is positive,
- * negative, or zero. It uses the srand() and rand() functions from the stdlib.h
+ * negative or zero. It uses the srand() and rand() functions from the stdlib.h
  * library to generate the random number, and then uses if-else statements to
- * determine whether it is positive, negative, or zero. The result is printed to
+ * determine whether it is positive, negative or zero. The result is printed to
  * the console using printf().
  *
  * Return: Always 0
