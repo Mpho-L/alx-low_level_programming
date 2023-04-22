f184a38530577abb3faf47fdcb6e983dc1226486
Mpho <mpholeqoalane@gmail.com>
2023-04-22 16:41:20 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This code declares a character array string and initializes
+ * it with the value "with proper grammar, but the outcome is a piece of art,".
+ * It then uses printf() to output the string to the console,
+ * followed by a newline character (\n)
+ * Return: Always 0 (Success)
+*/
+
+int main(void/0
+{
+char string = "with proper grammar, but the outcome is a piece of art,";
+pritnf("%s\n", string);
+
+return(0);
+}
