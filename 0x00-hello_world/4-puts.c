ecdcb72cf56fe2087143e766d1077d2dd7d77a6c
Mpho <mpholeqoalane@gmail.com>
2023-04-22 16:32:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ * This C code defines a main() function that declares
+ * a character array string and initializes it with the value
+ * "\"Programming is like building a multilingual puzzle".
+ * It then calls the puts() function with string as its argument,
+ * which outputs the string to the console.
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
 {
-char string[] = "Programming is like building a multilingual puzzle";
+char string[] = "\"Programming is like building a multilingual puzzle";
 puts(string);
 
 return (0);
