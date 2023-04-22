86dc6046caccbad1a5240445fb8335b8474ab33c
Mpho <mpholeqoalane@gmail.com>
2023-04-22 06:46:41 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/**This program that declares a constant string letter 
-*and then uses a while loop to print each character in the string to the console using the putchar() function.
+/**
+* main - Entry point for the program
+
+*This program that declares a constant string letter 
+*and then uses a while loop to print each character 
+*in the string to the console using the putchar() function.
 *The string letter is initialized to the lowercase alphabet, from 'a' to 'z'.
-*The const keyword is used to indicate that the contents of letter are read-only and cannot be modified during runtime.
+*The const keyword is used to indicate that the contents of 
+*letter are read-only and cannot be modified during runtime.
 *The while loop iterates over the characters in the string using an index variable i.
 *The loop continues as long as the current character is not the null character '\0'.
 *Inside the loop, the putchar() function is used to print each character to the console.
@@ -13,7 +18,8 @@
 int main() {
 const char* letter = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
-while (letter[i] != '\0') {
+while (letter[i] != '\0')
+{
 putchar(letter[i]);
 i++;
 }
