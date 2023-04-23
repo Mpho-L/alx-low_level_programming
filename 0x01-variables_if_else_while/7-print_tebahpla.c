a030ac8e651854575d3cd96c2bd90bc832eabe67
Mpho <mpholeqoalane@gmail.com>
2023-04-23 03:39:14 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,21 +4,21 @@
 *main - Entry point for the program
 *
 *Description: This program uses putchar() and a while loop to print the
-*letters of the alphabet in reverse order, 
-followed by a new line character.
+*letters of the alphabet in reverse order,
+*followed by a new line character.
 *
 *Return: Always 0 (Success)
 */
 
 int main(void)
 {
-const char *letter = "abcdefghijklmnopqrstuvwxyz\n";
+const char *letter = "\nabcdefghijklmnopqrstuvwxyz";
 int i = 0;
 while (letter[i] != '\0')
 {
 i++;
 }
-for (i =i - 1; i >= 0; i--)
+for (i = i - 1; i >= 0; i--)
 {
 putchar(letter[i]);
 }
