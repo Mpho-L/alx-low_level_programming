900b7c91068eb4c63d6e9e056801da7e1486684e
Mpho <mpholeqoalane@gmail.com>
2023-04-23 03:57:09 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - entry to the program
+ *
+ *Description: This program uses putchar() to print the numbers 0-9 and the
+ *letters A-F in hexadecimal, followed by a new line character.
+ *
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 16; i++)
+if (i < 10);
+{
+putchar(i + "0");
+}
+else
+{
+putchar(i - 10 + 'A');
+}
+putchar("\n");
+return (0);
+}
