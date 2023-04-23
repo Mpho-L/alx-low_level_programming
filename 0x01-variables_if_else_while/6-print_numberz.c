de2050b84b23538c895a4748db9ee69376ecbf41
Mpho <mpholeqoalane@gmail.com>
2023-04-23 02:24:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry to the program
+ *
+ *Description: This program declares an integer variable i and prints
+ *the numbers 0 to 9 to the console using a for loop and the putchar function.
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + "0");
+}
+putchar("\n");
+return (0);
+}
