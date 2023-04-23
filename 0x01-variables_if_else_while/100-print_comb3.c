9b79901311be6a1adca22bc9cb207229178889eb
Mpho <mpholeqoalane@gmail.com>
2023-04-23 10:20:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,8 +3,9 @@
 /**
  *main - entry to the program
  *
- *Description: This program prints all possible two-digit numbers separated
- *by commas.
+ *Description: This program prints all possible two-digit numbers
+ *separated by commas, with the smallest combination
+ *of digits where duplicates are excluded.
  *
  *Return: always 0 (success)
 */
@@ -21,9 +22,9 @@ putchar(i + '0');
 putchar(n + '0');
 if (i == 8 && n == 9)
 break;
-if (i != 9 || n != 9)
+//if (i != 9 || n != 9)
 putchar(',');
-if (i != 9 || n != 9)
+//if (i != 9 || n != 9)
 putchar(' ');
 }
 }
