062175932315c63b47c7d1350ed49d51c3ec0eb1
Mpho <mpholeqoalane@gmail.com>
2023-04-23 06:24:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - entry to the program
+ *
+ *Description: This program prints the digits 0 through 9 separated by commas
+ *
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 10)
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
