9e8d51fbecb40d8c386a8360706c57ee9082eb7f
Mpho <mpholeqoalane@gmail.com>
2023-04-23 09:38:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,16 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-		if (i != 9)
-			putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+putchar(',');
+if (i != 8)
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
