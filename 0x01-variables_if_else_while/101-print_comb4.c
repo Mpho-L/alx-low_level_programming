97a95922f19e35919a5a7e033efd4f56007e2b46
Mpho <mpholeqoalane@gmail.com>
2023-04-23 09:06:10 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - entry to the program
+ *
+ *Description: This program prints all possible three-digit numbers separated
+ *by commas.
+
+ *
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+int i, n, z;
+
+for (i = 0; i < 8; i++)
+{
+for (n = 1; n < 9; n++)
+{
+for (z = 2; z < 10; z++)
+{
+putchar(i + '0');
+putchar(n + '0');
+putchar(z + '0');
+
+if (i != 9 || n != 9 || z != 9)
+putchar(',');
+}
+}
+}
+putchar('\n');
+return (0);
+}
