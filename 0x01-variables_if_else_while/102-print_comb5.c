4cc304af74dc5fb16a06725425c615805a42e721
Mpho <mpholeqoalane@gmail.com>
2023-04-24 07:43:58 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,19 @@
 int main(void) {
 int i, j;
 
-for (i = 0; i < 100; i++) {
-for (j = i; j < 100; j++) {
-if (i != j) {
+for (i = 0; i < 100; i++) 
+{
+for (j = i; j < 100; j++)
+{
+if (i != j)
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i != 99 || j != 98) {
+if (i != 99 || j != 98)
+{
 putchar(',');
 putchar(' ');
 }
