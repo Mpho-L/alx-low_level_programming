adfdd6db2fd5faf5222c47d82db16debab8670f5
Mpho <mpholeqoalane@gmail.com>
2023-04-24 05:13:47 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,23 +9,21 @@
  *Return: always 0 (success)
 */
 
-#include <stdio.h>
-
 int main(void)
 {
 int i, n, z;
 
 for (i = 0; i < 8; i++)
 {
-for (n =i + 1; n < 9; n++)
+for (n = i + 1; n < 9; n++)
 {
-for (z =n + 1; z < 10; z++)
+for (z = n + 1; z < 10; z++)
 {
 putchar(i + '0');
 putchar(n + '0');
 putchar(z + '0');
 
-if (i != 9 || n != 9 || z != 9)
+if (i != 7 || n != 8 || z != 9)
 putchar(',');
 putchar(' ');
 }
