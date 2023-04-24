89f7fe7b05de5dfe25d21c4fce7c3e6901c99cbf
Mpho <mpholeqoalane@gmail.com>
2023-04-24 09:58:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the program
+ * 
+ *Description: The program prints a quote by Dora Korpar to the standard output.
+ *  
+ * returns 1
+*/
+int main(void)
+{
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int i = 0;
+while (quote[i] != '\0')
+{
+putchar(quote[i]);
+i++;
+}
+return (1);
+}
