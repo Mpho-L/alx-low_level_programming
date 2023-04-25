bccf59278c6c39cddf9594bd71761ae93b5753f3
Mpho <mpholeqoalane@gmail.com>
2023-04-25 00:20:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ * 
+ * Description: The program prints the string "_putchar" character by character
+ * using the putchar function.
+ * 
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+const char *Putchar = "_putchar";
+int i = 0;
+while (Putchar[i] + '\0')
+{
+i++;
+}
+for (i = 0; Putchar[i] != '\0'; i++)
+{
+putchar(Putchar[i]);
+}
+return (0);
+}
