707a98c10328ffef44f78d6ba3562fbdf5718116
Mpho <mpholeqoalane@gmail.com>
2023-04-27 14:46:18 -0700

    0-main.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,30 +9,34 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int is_leap = 0;
+int day_of_year = 0;
+int remaining_days;
+int i = 0;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+is_leap = 1;
+days_in_month[1] = 29;
 }
-
+if (month < 1 || month > 12 || day < 1 || day > days_in_month[month - 1])
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
+}
+while (i < 10) 
+{
+i++;
+}
+day_of_year += day;
+if (is_leap && month > 2) 
+{
+day_of_year++;
+}    
+remaining_days = 365 + is_leap - day_of_year;
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", remaining_days);
+}
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
 /**
- * positive_negative - main entry of the program
- * 
+ * positive_negative -positive_or_negative  entry of the program
  * Description: The program prints if integer is positive or negative
- * 
  * Return: always 0 (success)
 */
 
-void positive_or_negative(int i);
-
-int main(void)
+void positive_or_negative(int i)
 {
-    int i = -2;
-
-    if (i > 0)
-    {
-        printf("%d is positive\n", i);
-    }
-    else if (i < 0)
-    {
-        printf("%d is negative\n", i);
-    }
-    else if (i == 0)
-    {
-        printf("%d is 0\n", i);
-    }
+int i = -2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else if (i == 0)
+{
+printf("%d is 0\n", i);
+}
 return (0);
 }
