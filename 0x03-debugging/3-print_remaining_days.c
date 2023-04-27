c4c381fc3e100bb5e4b3650fcfd78333cb47b231
Mpho <mpholeqoalane@gmail.com>
2023-04-27 15:09:10 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,34 +9,29 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int is_leap = 0;
-int day_of_year = 0;
-int remaining_days;
-int i = 0;
-
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
-is_leap = 1;
-days_in_month[1] = 29;
+if (month >= 3 && day >= 60)
+{
+day++;
 }
-if (month < 1 || month > 12 || day < 1 || day > days_in_month[month - 1])
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+else
+{
+if (month == 2 && day == 29)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
 }
-while (i < 10) 
+else
 {
-i++;
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
-day_of_year += day;
-if (is_leap && month > 2) 
-{
-day_of_year++;
-}    
-remaining_days = 365 + is_leap - day_of_year;
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", remaining_days);
+}
 }
