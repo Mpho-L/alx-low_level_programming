7da076e58c83b1234f939d0e1dc618fd7116d503
Mpho <mpholeqoalane@gmail.com>
2023-05-04 18:31:18 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints a[2] = 98, followed by a new line
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int n;
@@ -7,7 +12,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-*p = 98; 
+*p = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
