20a25134e80558ca42fdf57969ae6e87d4a09f35
Mpho <mpholeqoalane@gmail.com>
2023-05-18 06:30:19 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -7,4 +7,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
-#endif
+#endif /*MAIN_H*/
