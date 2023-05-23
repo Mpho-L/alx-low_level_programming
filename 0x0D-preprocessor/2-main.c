346748eb0d0de8dee4808781e2cc4ded6383b33b
Mpho <mpholeqoalane@gmail.com>
2023-05-22 22:59:53 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - entry point to the program
+ * the program prints the name of the current source file
+ *
+ * Return: always 0 for success
+*/
+
 int main(void)
 {
 printf("%s\n", __FILE__);
-return 0;
+return (0);
 }
