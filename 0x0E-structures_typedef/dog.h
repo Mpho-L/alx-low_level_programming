1921aa3532e2c6ad018497baab665251e77ae30e
Mpho <mpholeqoalane@gmail.com>
2023-05-22 17:31:51 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,5 +10,7 @@ char *owner;
 
 void print_dog(dog_t *d);
 void init_dog(dog_t *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
