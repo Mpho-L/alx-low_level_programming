36c08bb1e5793f945e651af798a7283178f6e7b3
Mpho <mpholeqoalane@gmail.comgit config --global user.email mpholeqoalane@gmail.com>
2023-06-06 20:23:34 -0700

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * sum_listint - the program returns the sum of all the data (n) of a listint_t
+ * linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The sum of all the data (n) of the list, or 0 if the list is empty.
+ */
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+listint_t *current = head;
+
+while (current != NULL)
+{
+sum += current->n;
+current = current->next;
+}
+return sum;
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - the program inserts a new node at a given position
+ * in a listint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @idx: Index of the list where the new node should be added, starting at 0.
+ * @n: The value to be stored in the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new_node, *current;
+unsigned int i;
+
+if (head == NULL)
+return (NULL);
+
+if (idx == 0)
+return (add_nodeint(head, n));
+
+current = *head;
+
+for (i = 0; i < idx - 1; i++)
+{
+if (current == NULL)
+return (NULL);
+current = current->next;
+}
+if (current == NULL)
+return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = current->next;
+current->next = new_node;
+
+return (new_node);
+}
