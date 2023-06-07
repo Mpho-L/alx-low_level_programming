f843082f19d721ea222cbb38a1d1c8c516474d66
Mpho <mpholeqoalane@gmail.comgit config --global user.email mpholeqoalane@gmail.com>
2023-06-06 20:08:14 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - the program frees a listint_t list and sets the head to NULL.
+ * @head: Pointer to a pointer to the head of the list.
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *current;
+
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+current = *head;
+*head = (*head)->next;
+free(current);
+}
+}
