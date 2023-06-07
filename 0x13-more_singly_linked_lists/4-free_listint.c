a174eaf1d16e82b57a36dbdd7718ae13a0a4698c
Mpho <mpholeqoalane@gmail.comgit config --global user.email mpholeqoalane@gmail.com>
2023-06-06 20:02:36 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint - the program frees a listint_t list.
+ * @head: Pointer to the head of the list.
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *current;
+
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
+}
