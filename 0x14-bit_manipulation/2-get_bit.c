8677227a999a8fa69f5cfb6903eb0065114980f9
Mpho <mpholeqoalane@gmail.comgit config --global user.email mpholeqoalane@gmail.com>
2023-06-08 10:16:05 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 #include <stddef.h>
+#include "main.h"
 
 /**
- * set_bit - the program sets the value of a bit to 1 at a given index.
- * @n: Pointer to the number to modify.
- * @index: The index of the bit to set.
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The number to retrieve the bit from.
+ * @index: The index of the bit to get.
  *
- * Return: 1 if it worked, or -1 if an error occurred.
+ * Return: The value of the bit at the given index, or -1 if an error occurred.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int mask;
-unsigned long int bit;
+int bit;
 if (index >= (sizeof(unsigned long int) * 8))
-{
 return (-1);
-}
+
 mask = 1UL << index;
-bit = n & mask;
-return ((bit != 0) ? 1 : 0);
+bit = (n & mask) >> index;
+
+return (bit);
 }
