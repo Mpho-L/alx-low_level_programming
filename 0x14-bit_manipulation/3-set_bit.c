935154356218bee39353671c5a1322e77aec862c
Mpho <mpholeqoalane@gmail.comgit config --global user.email mpholeqoalane@gmail.com>
2023-06-08 08:36:21 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * set_bit - the program sets the value of a bit to 1 at a given index.
+ * @n: Pointer to the number to modify.
+ * @index: The index of the bit to set.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1);
+
+unsigned long int mask = 1UL << index;
+*n |= mask;
+
+return (1);
+}
